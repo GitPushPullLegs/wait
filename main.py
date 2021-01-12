3652239c6f4049ef82ba8807508a8340f423bb6e
GitPushPullLegs <jose.aguilar.6694@gmail.com>
2021-01-11 17:01:28 -0800

    Initial Commit

--- main.py
+++
@@ -1,7 +0,0 @@
-import wait
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    ww = wait.BrowserWait()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
