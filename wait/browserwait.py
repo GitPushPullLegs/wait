6807bb73f75a0dd14bc334126e835d96eb07c954
GitPushPullLegs <jose.aguilar.6694@gmail.com>
2021-02-01 21:50:38 -0800

    Renamed browser to driver.

--- wait/browserwait.py
+++ wait/browserwait.py
@@ -3,9 +3,9 @@ from .wait import Wait
 
 class BrowserWait(Wait):
 
-    def __init__(self, browser, **kwargs):
+    def __init__(self, driver, **kwargs):
         super().__init__(**kwargs)
-        self.browser = browser
+        self.driver = driver
 
     def until_angular_loads(self):
         """Returns when an AngularJS page loads completely."""
@@ -13,7 +13,7 @@ class BrowserWait(Wait):
 
     def _is_angular_loaded(self):
         """Returns true if there are no pending http requests. Angular lists all network requests in a list."""
-        return self.browser.execute_script("""
+        return self.driver.execute_script("""
             return angular.element(document).injector().get('$http').pendingRequests.length === 0
         """)
 
@@ -24,8 +24,8 @@ class BrowserWait(Wait):
     def _is_file_downloaded(self):
         """Checks Google Chrome's downloads page to see if the latest element's state is 'COMPLETE' if so,
         returns the file path. """
-        self.browser.get('chrome://downloads')
-        return self.browser.execute_script("""
+        self.driver.get('chrome://downloads')
+        return self.driver.execute_script("""
         var items = document.querySelector('downloads-manager')
             .shadowRoot.querySelector('#downloadsList').items;
         var latestDownload = items[0]
